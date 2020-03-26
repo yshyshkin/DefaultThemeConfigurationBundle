acce8f0a656aad6c48a9bb9e53f7f30f7d434d6c
Yevhen Shyshkin <yshyshkin@oroinc.com>
2020-03-26 19:08:48 +0100

    Button and icon colors  - added configuration for button colors  - added configuration for icon colors

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,16 +19,22 @@ class Configuration implements ConfigurationInterface
         SettingsBuilder::append(
             $rootNode,
             [
-                'company_name'              => ['type' => 'string', 'value' => null],
-                'company_logo'              => ['value' => null],
-                'header_footer_bg_color'    => ['type' => 'string', 'value' => null],
-                'header_footer_text_color'  => ['type' => 'string', 'value' => null],
-                'main_menu_bg_color'        => ['type' => 'string', 'value' => null],
-                'main_menu_text_color'      => ['type' => 'string', 'value' => null],
-                'secondary_menu_bg_color'   => ['type' => 'string', 'value' => null],
-                'secondary_menu_text_color' => ['type' => 'string', 'value' => null],
-                'table_header_bg_color'     => ['type' => 'string', 'value' => null],
-                'table_header_text_color'   => ['type' => 'string', 'value' => null],
+                'company_name'                  => ['type' => 'string', 'value' => null],
+                'company_logo'                  => ['value' => null],
+                'header_footer_bg_color'        => ['type' => 'string', 'value' => null],
+                'header_footer_text_color'      => ['type' => 'string', 'value' => null],
+                'main_menu_bg_color'            => ['type' => 'string', 'value' => null],
+                'main_menu_text_color'          => ['type' => 'string', 'value' => null],
+                'secondary_menu_bg_color'       => ['type' => 'string', 'value' => null],
+                'secondary_menu_text_color'     => ['type' => 'string', 'value' => null],
+                'table_header_bg_color'         => ['type' => 'string', 'value' => null],
+                'table_header_text_color'       => ['type' => 'string', 'value' => null],
+                'main_button_bg_color'          => ['type' => 'string', 'value' => null],
+                'main_button_text_color'        => ['type' => 'string', 'value' => null],
+                'secondary_button_bg_color'     => ['type' => 'string', 'value' => null],
+                'secondary_button_text_color'   => ['type' => 'string', 'value' => null],
+                'icon_bg_color'                 => ['type' => 'string', 'value' => null],
+                'icon_content_color'            => ['type' => 'string', 'value' => null],
             ]
         );
 
